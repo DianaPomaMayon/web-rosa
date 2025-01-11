770c747ee71a2644f7bcd3f9a71e9f0b03975b1a
DianaPomaMayon <pomadiana295@gmail.com>
2025-01-10 21:27:34 -0500

    se agregaron mas clases

--- rosa_bella/lib/model/registro_model.dart
+++ rosa_bella/lib/model/registro_model.dart
@@ -12,3 +12,63 @@ class RegistroModel {
         );
   }
 }
+
+class RegistroModel {
+  var nombreUsuaurio;
+  var celular;
+  var password;
+
+  RegistroModel({this.nombreUsuaurio, this.celular, this.password});
+  factory RegistroModel.fromJson(Map<String, dynamic> json) {
+    return RegistroModel(
+        nombreUsuaurio: json['nombreUsuaurio'],
+        celular: json['celular'],
+        password: json['password'],
+        );
+  }
+}
+
+class RegistroModel {
+  var nombreUsuaurio;
+  var celular;
+  var password;
+
+  RegistroModel({this.nombreUsuaurio, this.celular, this.password});
+  factory RegistroModel.fromJson(Map<String, dynamic> json) {
+    return RegistroModel(
+        nombreUsuaurio: json['nombreUsuaurio'],
+        celular: json['celular'],
+        password: json['password'],
+        );
+  }
+}
+
+class RegistroModel {
+  var nombreUsuaurio;
+  var celular;
+  var password;
+
+  RegistroModel({this.nombreUsuaurio, this.celular, this.password});
+  factory RegistroModel.fromJson(Map<String, dynamic> json) {
+    return RegistroModel(
+        nombreUsuaurio: json['nombreUsuaurio'],
+        celular: json['celular'],
+        password: json['password'],
+        );
+  }
+}
+
+class RegistroModel {
+  var nombreUsuaurio;
+  var celular;
+  var password;
+
+  RegistroModel({this.nombreUsuaurio, this.celular, this.password});
+  factory RegistroModel.fromJson(Map<String, dynamic> json) {
+    return RegistroModel(
+        nombreUsuaurio: json['nombreUsuaurio'],
+        celular: json['celular'],
+        password: json['password'],
+        );
+  }
+}
